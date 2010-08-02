55f458487cf0a026e1faf04c7931385990775d2c
Steve Kondik <steve@cyngn.com>
2010-08-02 11:23:47 -0700

    Forward-port mkbootimg / unpackbootimg support
     * Mostly from Koush and Seth Shelnutt
    
    unpackbootimg (squashed)
    
    unpackbootimg ported forward from eclair
    
    Change-Id: I74d2df0b47d40e7105cc58c2b05f8f383dc7f8a0
    
    port forward pagesize arg from eclair
    
    Change-Id: Ia789a4f392a3890aa0efa7efb42032482b48beb0
    
    unpackbootimg should output BOARD_PAGE_SIZE
    
    Change-Id: Ieb5cda01943a33da97eee4d025f56c2c6e7560e8
    
    output page size in mkbootimg
    
    add an option to override the pagesize for boot images created by nubs. ie, acer liquid mkliquidbootimg.
    
    Change-Id: Ie0c7e67edf5ae59019517e72b9be8c0b81388e41
    
    Update unpackbootimg to verify the Android boot image magic value. It will also search the first 512 bytes for padding.
    
    Change-Id: I490cba05f2bb616a3f64e3271ecaa61eb9e64be8
    
    unpackbootimg: Fix up the padding search in case the magic does not fall on a 8 byte boundary.
    
    Change-Id: I57471f9c2117cd7965b6958ea0aa88e356436da6
    
    unpackbootimg: Fix magic search.
    
    Change-Id: I68470b637556a08e48ff72b7ef8811cba13b04ad
    
    unpackbootimg: apparently mkbootimg no longer accepts hex values
    
    Change-Id: I95a33f7b40470e4500d418d863a65a75e7aa8499
    
    unpackbootimg: Need to also update the written file.
    
    Change-Id: I45faddbae85273c79b2837f97933634b6e70546f
    
    ramdiskaddr is now ramdisk_offset
    
    Change-Id: I3bf83af5f7001f581506dc7fd9b1eb653334ad35
    
    unpackbootimg: remove host LOCAL_MODULE_TAG
    
    Change-Id: I199d680dc5ab8bf50f5be65c29095bf3adade695
    
    unpackbootimg: Add support for device trees
    
    Change-Id: I340eed99d2274a2f4cbaf5a9f27726ff3a9302e5
    
    unpackbootimg: Add support for detecting lz4 compressed ramdisks
    
    This will check for lz4 magic, and if found change the extension of the
    file. Else it'll fall back to the default behavior of assuming gzip. This has
    been tested with stock LS980 boot images and LS970 boot images for lz4
    and gzip respectively.
    
    Change-Id: If2139ff172397b6db079ffb7ab9cb61897c38fb3
    
    unpackbootimg: Add support for dumping ramdisk offset
    
    Change-Id: Ic62b9fe61db4435ecbc52b66db5ffc9b9d79cbb4
    
    unpackbootimg: Add support for second, second_offset and tags_offset.
    
    Change-Id: Ia7ef7f00191dbf2c44736c4e4d980f72afa8c253
    
    Conflicts:
    	mkbootimg/mkbootimg.c

--- mkbootimg/mkbootimg.c
+++ mkbootimg/mkbootimg.c
@@ -66,6 +66,9 @@ int usage(void)
             "       [ --board <boardname> ]\n"
             "       [ --base <address> ]\n"
             "       [ --pagesize <pagesize> ]\n"
+            "       [ --dt <filename> ]\n"
+            "       [ --ramdisk_offset <address> ]\n"
+            "       [ --tags_offset <address> ]\n"
             "       [ --id ]\n"
             "       -o|--output <filename>\n"
             );
